0f735f21c828dc487ae44a913fc3b851492f027b
unknown <ermal.tufa@moveoalbania.com>
2021-09-08 18:02:00 +0200

    New Carousel Material UI in Home Scr

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -12,6 +12,7 @@ import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import BannerExample from '../components/CarouselMaterial';
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
@@ -46,7 +47,7 @@ export default function HomeScreen() {
   return (
     <div>
       <h2>Top Sellers</h2>
-      {loadingSellers ? (
+      {/* {loadingSellers ? (
         <LoadingBox></LoadingBox>
       ) : errorSellers ? (
         <MessageBox variant="danger">{errorSellers}</MessageBox>
@@ -64,7 +65,8 @@ export default function HomeScreen() {
             ))}
           </Carousel>
         </>
-      )}
+      )} */}
+      <BannerExample/>
       <h2>Featured Products</h2>
       {loading ? (
         <LoadingBox></LoadingBox>
